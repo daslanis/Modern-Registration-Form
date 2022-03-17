f5d6545938c97b341b4769f055c30421d1909d36
Dimitrios Aslanis <38957590+daslanis@users.noreply.github.com>
2022-03-17 21:18:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function register()
+{
+  window.alert("FAILED TO REGISTER.");
+  var element = document.getElementById(register_form);
+  console.log(element.value);
+}
+
+  /*
+function swap(a)
+{
+
+  if(a === 'login'){
+    window.alert("test")
+  }
+  else{
+  window.alert(a);
+}}
+
+*/
